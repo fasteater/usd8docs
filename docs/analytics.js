0a34ae15f573f6c911eccdeebb65b11dda74fc34
fastEater <rickchenn@gmail.com>
2025-12-09 16:46:11 +0100

    adding analytics

--- docs/analytics.js
+++ docs/analytics.js
@@ -1,9 +1,10 @@
 //here we manually creates tags and insert the js into the final html.
 
 //for google analytics
+var gaMeasurementId = 'G-XZ3M0DQJ6M';
 var s = document.createElement('script');
 s.async = true;
-s.src = 'https://www.googletagmanager.com/gtag/js?id=G-L93KXXWJFX';
+s.src = 'https://www.googletagmanager.com/gtag/js?id=' + gaMeasurementId;
 document.head.appendChild(s);
 
 var s2 = document.createElement('script');
@@ -11,7 +12,7 @@ s2.innerHTML = `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-L93KXXWJFX');
+gtag('config', '${gaMeasurementId}');
 `;
 document.head.appendChild(s2);
 

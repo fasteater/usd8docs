4acafa338b587968e1bc36b4ba55a63fc709b248
fastEater <rickchenn@gmail.com>
2025-12-09 16:49:19 +0100

    update

--- docs/analytics.js
+++ docs/analytics.js
@@ -15,12 +15,3 @@ gtag('js', new Date());
 gtag('config', '${gaMeasurementId}');
 `;
 document.head.appendChild(s2);
-
-//for twitter pixel
-var s3 = document.createElement('script');
-s3.innerHTML = `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
-a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-twq('config','qhzoq');
-`;
-document.head.appendChild(s3);
